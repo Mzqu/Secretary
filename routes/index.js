ca23fa84bef5e909b9c5ab20265eb04d3d977eed
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2016-04-23 16:22:07 -0700

    dicks

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// router.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+router.post('/api/messages', bot.verifyBotFramework(), bot.listen());
 // router.post('/api/messages', bot.verifyBotFramework(), bot.listen());
 
 module.exports = router;
