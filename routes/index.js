cacb9c89df7e4860956ad0e7aedc59054ba5ccbd
Michael <mzqu98>
2016-04-23 21:25:13 -0700

    backup

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var utils = require('../util.js');
 var builder = require('botbuilder');
 var twilio = require('twilio');
 
-var model = 'https://api.projectoxford.ai/luis/v1/application?id=f86def56-4901-4874-89c1-19f43e9e7d9d&subscription-key=9b8a3f03610f4c5aa8f07e1b31664d27';
+var model = 'https://api.projectoxford.ai/luis/v1/application?id=33892828-8dde-4383-9efa-8d4246fc5a2c&subscription-key=9b8a3f03610f4c5aa8f07e1b31664d27';
 var bot = new BCBot(model, 'TextBot');
 var twilioMessage = '';
 var cortanaBot = bot.bot;
@@ -15,11 +15,12 @@ cortanaBot.add('/', dialog);
 
 bot.addCommand(new utils.BotCommandFactory('set temperature', [
 	function(session, args, next) {
-		if (builder.EntityRecognizer.findEntity(args.entities, 'thermometer::target temperature') || builder.EntityRecognizer.findEntity(args.entities, 'filefolder::name')) {
+		if (builder.EntityRecognizer.findEntity(args.entities, 'builtin.temperature')) {
 			twilioMessage = JSON.stringify(args.entities);
 			finished = true;
 			session.send("lmao");
 		} else {
+      finished = true;
 			session.send("no entities found");
 		}
 	},
@@ -32,11 +33,13 @@ bot.addCommand(new utils.BotCommandFactory('set temperature', [
 ]));
 bot.addCommand(new utils.BotCommandFactory('texting'), [
   function(session, args, next) {
-    if (builder.EntityRecognizer.findEntity(args.entities, 'responseMessage')) {
+    if (builder.EntityRecognizer.findEntity(args.entities, 'texting')) {
       twilioMessage = JSON.stringify(args.entities);
+      // callbackerino("texting");
       finished = true;
       session.send("lmao");
     } else {
+      finished = true;
       session.send("no entity found");
     }
   },
@@ -49,11 +52,12 @@ bot.addCommand(new utils.BotCommandFactory('texting'), [
 ]);
 bot.addCommand(new utils.BotCommandFactory('calling'), [
   function(session, args, next) {
-    if (builder.EntityRecognizer.findEntity(args.entities, 'responseMessage')) {
+    if (builder.EntityRecognizer.findEntity(args.entities, 'calling')) {
       twilioMessage = JSON.stringify(args.entities);
       finished = true;
       session.send("lmao");
     } else {
+      finished = true;
       session.send("no entity found");
     }
   },
@@ -64,7 +68,10 @@ bot.addCommand(new utils.BotCommandFactory('calling'), [
     session.send("???");
   }
 ]);
-bot.setDefault(new utils.BotDefaultCommandFactory(builder.DialogAction.send("I'm sorry I didn't understand. I can only create and delete alarms.")));
+bot.setDefault(new utils.BotDefaultCommandFactory(function() {
+  builder.DialogAction.send("I'm sorry I didn't understand. I can only create and delete alarms.");
+  finished = true;
+}));
 
 router.get('/', function(req, res) {
 	var twiml = new twilio.TwimlResponse();
@@ -84,42 +91,109 @@ router.get('/', function(req, res) {
 router.post('/', function(req, res) {
 	var twiml = new twilio.TwimlResponse();
 	console.log(req.body.From);
-	cortanaBot.processMessage({
-		text: req.body.Body || ''
-	});
-	var testerino = setInterval(function() {
-		console.log("running");
-		if (finished == true) {
-      var json = JSON.parse(twilioMessage);
-			console.log("finished");
-			finished = !finished;
-			console.log(twilioMessage);
-      twiml.message(smsManager(twilioMessage));
-			if (json[0].type == 'texting') {
-        console.log("actally in the if");
-				const spawn = require('child_process').spawn;
-				const sleep = spawn('sleep', [json[0].entity]);
-				sleep.on('close', function() {
-					sendSMS(req.body.From, 'alarm up!');
-				});
-			}
-			res.writeHead(200, {
-				'Content-Type': 'text/xml'
-			});
-			res.end(twiml.toString());
-			clearInterval(testerino);
-		}
-	}, 100);
+  if (req.body.Body.substring(0, 5).toLowerCase() == "open ") {
+    var exec = require('child_process').exec;
+    var child = exec('open -a "' + req.body.Body.substring(5, req.body.Body.length) + '"', function (error, stdout, stderr) {
+      if (error == null) {
+        sendSMS(req.body.From, "Opened " + req.body.Body.substring(5, req.body.Body.length));
+      } else {
+        sendSMS(req.body.From, "Error opening " + req.body.Body.substring(5, req.body.Body.length));
+      }
+    });
+  } else if (req.body.Body.substring(0, 5).toLowerCase() == "exit ") {
+    console.log("exit");
+    var exec = require('child_process').exec;
+    var child = exec("ps -ax | awk '/[" + req.body.Body.charAt(5) + "]" + req.body.Body.substring(6, req.body.Body.length) + "/{print $1}'", function (error, stdout, stderr) {
+      var allProcesses = stdout.split('\n');
+      console.log(allProcesses);
+      for (var i = 0; i < allProcesses.length - 1; i++) {
+        console.log(allProcesses[i]);
+        var child2 = exec("kill " + allProcesses[i], function (e, o, se) {
+          if (e == null) {
+            sendSMS(req.body.From, "Exited " + req.body.Body.substring(5, req.body.Body.length));
+          } else {
+            sendSMS(req.body.From, "Error exiting " + req.body.Body.substring(5, req.body.Body.length));
+          }
+        });
+      }
+    });
+  } else if (req.body.Body.toLowerCase() == "sleep") {
+    var exec = require('child_process').exec;
+    var child = exec('pmset sleepnow', function (e, o, se) {
+      if (e == null) {
+        sendSMS(req.body.From, "Put computer to sleep.");
+      } else {
+        sendSMS(req.body.From, "Error putting computer to sleep.");
+      }
+    });
+  } else {
+  	cortanaBot.processMessage({
+  		text: req.body.Body || ''
+  	});
+  	var testerino = setInterval(function() {
+  		console.log("running");
+  		if (finished == true) {
+        try {
+          var json = JSON.parse(twilioMessage);
+    			console.log(json);
+    			finished = !finished;
+    			console.log(twilioMessage);
+          twiml.message(smsManager(twilioMessage));
+    			if (json[0].type == "texting") {
+    				var spawn = require('child_process').spawn;
+    				var sleep = spawn('sleep', [parseTime(json[1].resolution.duration)]);
+    				sleep.on('close', function() {
+    					sendSMS(req.body.From, json[0].entity);
+    				});
+    			} else if (json[0].type == "calling") {
+            var spawn = require('child_process').spawn;
+    				var sleep = spawn('sleep', [parseTime(json[1].resolution.duration)]);
+    				sleep.on('close', function() {
+    					sendSMS(req.body.From, json[0].entity);
+    				});
+          }
+    			res.writeHead(200, {
+    				'Content-Type': 'text/xml'
+    			});
+    			res.end(twiml.toString());
+        } catch (e) {
+          console.log(e.message);
+          sendSMS(req.body.From, "Command not recognized.");
+        } finally {
+  			  clearInterval(testerino);
+          twilioMessage = "";
+        }
+  		}
+  	}, 100);
+  }
 });
+
+function parseTime(time) {
+  var matches = time.substring(2, time.length).match(/[a-zA-Z]+|[0-9]+/g);
+  var seconds = 0;
+  for (var i = 0; i < matches.length; i++) {
+      if (matches[i + 1] == "H") {
+        seconds += matches[i] * 60 * 60;
+        i++;
+      } else if (matches[i + 1] == "M") {
+        seconds += matches[i] * 60;
+        i++;
+      } else {
+        seconds += matches[i] * 1;
+        return seconds;
+      }
+  }
+  return seconds;
+}
 
 function smsManager(message) {
 	var json = JSON.parse(message);
-	if (json.type = "thermometer") {
+	if (json[0].type == "builtin.temperature") {
 		return "Setting thermometer temperature to " + json[0].entity;
-	} else if (json.type = "texting") {
-		return "not implemented";
-	} else if (json.type = "calling") {
-    return "not implemented";
+	} else if (json[0].type == "texting") {
+		return "Got it! Texting you in " + json[1].resolution.duration;
+	} else if (json[0].type == "calling") {
+    return "Got it! Calling you in " + json[1].resolution.duration;
   } else {
 		return message;
 	}
